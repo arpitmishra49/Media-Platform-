b3aeffb447af99d9a1690ec9f79da3589b1fc30d
arpitmishra49 <arpitmishra4925@gmail.com>
2026-02-17 15:41:30 +0530

    Made some changes in the shimmer card

--- components/ShimmerCard.jsx
+++ components/ShimmerCard.jsx
@@ -2,10 +2,10 @@ const ShimmerCard = () => {
     return (
       <div className="bg-gray-800 rounded-xl overflow-hidden animate-pulse">
         
-        {/* Thumbnail */}
+        
         <div className="h-40 bg-gray-700"></div>
   
-        {/* Text section */}
+        
         <div className="p-4 space-y-3">
           <div className="h-4 bg-gray-700 rounded w-3/4"></div>
           <div className="h-4 bg-gray-700 rounded w-1/2"></div>
